773b0ca5dd6ce5c95a66e115dd16eaa82322dd8d
aldoranx <geraldwork1@gmail.com>
2021-07-05 15:28:34 +0200

    ASG

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region = "us-east-2"
 
 }
+
+data "aws_vpc" "default" {
+    default = true
+  }
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
--- resources.tf
+++ resources.tf
@@ -1,9 +1,44 @@
-resource "aws_instance" "example" {
-  ami = "ami-00399ec92321828f5"
-  instance_type = "t2.micro"
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-00399ec92321828f5"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-  tags = {
-    name = "Terraform-example"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello Ranx" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
   }
+
   
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
